5ecd9399f33d1cd0f91ceb3910710229ee0dd41c
j-256 <james@jklein.dev>
2026-03-25 23:44:57 +0000

    Initial scaffolding

---
+++ src/extension.ts
@@ -0,0 +1,9 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext): void {
+  // Stub -- will be implemented in later tasks
+}
+
+export function deactivate(): void {
+  // Nothing to clean up
+}
